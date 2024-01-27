54720d9e47aa57316de1d0b532224635dd6658c8
vEDHAT23 <157867023+vEDHAT23@users.noreply.github.com>
2024-01-27 15:02:47 +0530

    BACK1.PY
    EVOLOL

--- back1.py
+++ back1.py
@@ -1,1 +1,36 @@
+print("   Welcome to Python World!!!   ")
+print('_'*30)
 
+
+#print the rules.
+print()
+print()
+
+data = {}
+ 
+with open("Login.txt","r") as test:
+    lines=test.readlines()#all lines
+    for line in lines: # slicing one line at a time
+        n,p=line.strip().split(':') #split() using which we are splitting n & p
+        data[n]=p # n= key , p=value in data dict
+
+while True:
+    ans=input("Enter your name to login :")
+
+    if ans in data.keys():
+        print("Hi",ans)
+        in_p=input("Please type you password : ")#1234 
+        if in_p.lower() == data[ans].lower():#data[ans]->dict[key]=value
+            print("Welcome to python world")
+
+            break
+            
+    
+        else:
+            
+            print("Incorrect password")
+    else:
+        print("Invalid Username")
+
+else:
+    print("Please try entering one or 2")
