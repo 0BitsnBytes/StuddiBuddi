9f9c8af38c7df2f03d60b5dd930a82a6320adc70
0BitsnBytes <157201615+0BitsnBytes@users.noreply.github.com>
2024-01-28 15:34:44 +0530

    Flask v3
    hhh

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_session import Session
 from bardapi import Bard
 import requests , json
 
+x = None
+
 Username = ["Samarth","Mudith"]
 Password = ["is","Einstien2.0"]
 
@@ -31,20 +33,30 @@ def process():
   
 @app.route("/query", methods=["POST"])
 def Query():
-  url = "https://neutrinoapi-bad-word-filter.p.rapidapi.com/bad-word-filter"
+  url = "https://profanity-cleaner-bad-word-filter.p.rapidapi.com/profanity"
+  x = request.form["Query"].strip()
   payload = {
-	  "content": request.form["Query"],
-	  "censor-character": "*"}
+    "text": x,
+    "maskCharacter": "*",
+    "language": "en"
+  }
   headers = {
-	  "X-RapidAPI-Key": "8631403470mshd95960d1294af1ap1b37cbjsn5c0a2d143b60",
-	  "X-RapidAPI-Host": "neutrinoapi-bad-word-filter.p.rapidapi.com"}
-  response = requests.post(url, data=payload, headers=headers).json()
-  bad = response["is-bad"]
-  
-  if bad == True:
-    return render_template("homepageIsBad.html")
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "8631403470mshd95960d1294af1ap1b37cbjsn5c0a2d143b60",
+    "X-RapidAPI-Host": "profanity-cleaner-bad-word-filter.p.rapidapi.com"
+  }
+
+  response = requests.post(url, json=payload, headers=headers).json()
+  if not response["profanities"]:
+    Var=(Bard(token = "fwilwx1fjOC0z04elgwEaJiQzLdSjuj2k04L3SH06M2_XZBmfLd43gdLGDIj6R21eCW0ig.")).get_answer(f'Give a hint for the answer to the question in one line {x}. Do not give the answer.')
+    return render_template("answers.html",Var=Var["content"],aer=x)
   else:
-    return ((Bard(token = "fwilwx1fjOC0z04elgwEaJiQzLdSjuj2k04L3SH06M2_XZBmfLd43gdLGDIj6R21eCW0ig.")).get_answer(f'Give a hint for the answer to the question in one line {request.form["Query"]}. Do not give the answer.'))["content"] 
+    return render_template("homepageIsBad.html")
+  
+@app.route("/proc",methods=["POST"])
+def proc():
+  x = request.form["Val"]
+  return render_template("answer.html",Var = (Bard(token = "fwilwx1fjOC0z04elgwEaJiQzLdSjuj2k04L3SH06M2_XZBmfLd43gdLGDIj6R21eCW0ig.")).get_answer(f'Give a answer to the question "{x}" in 1 line only as text')["content"])
 
 
 app.run(debug=True)
