c81b39c0e886c8cf1d9bd3ea754f24c3b24cebb7
0BitsnBytes <157201615+0BitsnBytes@users.noreply.github.com>
2024-01-28 10:08:35 +0530

    Flask Is hard
    hhhh

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template, redirect, session, request
 from flask_session import Session
 
 app = Flask(__name__)
-Passwords = ""
+app.secret_key = "BroLikesSchoolFood#6942"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
+def index():
+  return redirect("/Login")
+
+@app.route("/Login")
+@app.route("/login")
 def Login():
-  return "hello"
+  return render_template("login.html")
+
+@app.route("/process" , methods=["POST"])
+def process():
+  Value = request.form
+  return Value
 
 app.run(debug=True)
